d40ebea88835fb9ab9022e4574655d5fd6d86334
Rwanee <erwan.ababsa@epitech.eu>
2025-02-09 13:47:25 +0100

    sound + fix map + illusion

--- coin.gd
+++ coin.gd
@@ -1,7 +1,9 @@
 extends Area3D
 
 const ROT_SPEED = 2 # number of degrees the coin rotates every frame
-
+var sounds = {
+	"coin": preload("res://coinsound.mp3"),
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +14,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	rotate_y(deg_to_rad(ROT_SPEED))
 
+func play_sound(name):
+	if name in sounds:
+		$AudioStreamPlayer.stream = sounds[name]
+		$AudioStreamPlayer.play()
+
 func _on_body_entered(body: Node3D) -> void:
 	Global.coins += 1
 	set_collision_layer_value(3, false)
@@ -19,6 +26,11 @@ func _on_body_entered(body: Node3D) -> void:
 	if Global.coins >= 3:
 		get_tree().change_scene_from_file()
 	$AnimationPlayer.play("bounce")
+	play_sound("coin")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	queue_free()
+
+
+func _on_audio_stream_player_child_entered_tree(node: Node) -> void:
+	pass # Replace with function body.
